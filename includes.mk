68f0ddfa07efc06323ed8a4b363638b1da051611
Oleg Dolya <oleg.dolya@gmail.com>
2014-09-20 14:44:45 +0300

    fix bug

--- includes.mk
+++ includes.mk
@@ -5,8 +5,8 @@ define build_tarmaker
 	@sudo docker cp builder-${NAME}:/tmp/rootfs.tar.md5 ./
 	@sudo chown 1000:1000 rootfs*
 	@sudo docker rm -f builder-${NAME}
-  @echo ---------------------------------
-  @echo image: tarmaker:${NAME} size: $(shell sudo docker images | grep $(IMAGE_NAME) | grep $(TAG_IMG) | awk '{print $$7 $$8}')
+	@echo ---------------------------------
+	@echo image: tarmaker:${NAME} size: $(shell sudo docker images | grep $(IMAGE_NAME) | awk '{print $$7 $$8}')
 endef
 
 check-rootfs:
