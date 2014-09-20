419e656c4d6e3772e8c42ee70636124bc52c7ed0
Oleg Dolya <oleg.dolya@gmail.com>
2014-09-20 12:52:22 +0300

    modify lsyncd

---
+++ lsyncd/includes.mk
@@ -0,0 +1,15 @@
+ifndef NAME
+  NAME = mini
+endif
+
+ifndef TAG_IMG
+  TAG_IMG = latest
+endif
+
+ifndef IMAGE_REPO
+  IMAGE_REPO = grengojbo
+endif
+
+ifndef IMAGE_NAME
+  IMAGE_NAME = $(IMAGE_REPO)/$(NAME)
+endif
