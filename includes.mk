ad515607318958183ac196fd01ef98ca9b5b84e7
Oleg Dolya <oleg.dolya@gmail.com>
2014-09-23 23:27:13 +0300

    modify Makefile

--- includes.mk
+++ includes.mk
@@ -6,7 +6,7 @@ define build_tarmaker
 	@sudo chown 1000:1000 rootfs*
 	@sudo docker rm -f builder-${NAME}
 	@echo ---------------------------------
-	@echo image: tarmaker:${NAME} size: $(shell sudo docker images | grep $(IMAGE_NAME) | awk '{print $$7 $$8}')
+	@echo image: tarmaker:${NAME} size: $(shell sudo docker images | grep $(NAME) | grep $(TAG_IMG) | awk '{print $$7 $$8}')
 endef
 
 check-rootfs:
